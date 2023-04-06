7d9be7f7e49ed2dc24c4da1718c5c9c163b57762
unknown <gallardoirg@gmail.com>
2023-04-06 08:33:32 -0600

    Initial Commit

---
+++ pointclouds/index/cloud.js
@@ -0,0 +1,59 @@
+{
+    "version": "1.8",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 107317514,
+    "boundingBox": {
+        "lx": 533792.36075,
+        "ly": 1680655.4769,
+        "lz": -0.756,
+        "ux": 535250.55275,
+        "uy": 1682113.6689,
+        "uz": 1457.436000000039
+    },
+    "tightBoundingBox": {
+        "lx": 533792.36075,
+        "ly": 1680655.4769,
+        "lz": -0.756,
+        "ux": 535192.59375,
+        "uy": 1682113.6689,
+        "uz": 34.574
+    },
+    "pointAttributes": [
+        {
+            "name": "POSITION_CARTESIAN",
+            "size": 12,
+            "elements": 3,
+            "elementSize": 4,
+            "type": "int32",
+            "description": ""
+        },
+        {
+            "name": "RGBA",
+            "size": 4,
+            "elements": 4,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "classification",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        },
+        {
+            "name": "return number",
+            "size": 1,
+            "elements": 1,
+            "elementSize": 1,
+            "type": "uint8",
+            "description": ""
+        }
+    ],
+    "spacing": 12.628313064575196,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
